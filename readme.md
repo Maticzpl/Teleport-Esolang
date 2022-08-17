ee6b90fd93a6dfc8cda718934d64a60cf417a005
Mateusz <maticzpl06@gmail.com>
2022-08-17 18:32:14 +0200

    Added debugger

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 ## How to run stuff?
 First run `npm install` in this folder.  
 Then run `node . <path to file>`  
-For example `node . ./fibonacci.factory`  
-or `node . ./arrayEditor.factory`  
+For example `node . ./fibonacci.telep`  
+or `node . ./arrayEditor.telep`  
 
 # How do I write it?
 ## Basic function
@@ -98,8 +98,128 @@ This makes it possible to fetch the value from anywhere using `<- #get`.
 
 When the signal reaches `| -> #set` it already has the value of 10. It teleports back up to the value block `[5]` and since the signal value isn't NULL, it overwrites the value block with the number 10 and gives the `#get` teleport the value of 10. Afterwards it returns to the bottom part to reach `<print>`. The value doesn't persist when you return.
 
+### Conditionals
+The `?` symbol is used for conditional jumps.
+Example:
+```
+!
+[1]
+? -> #jump // This one will be printed
+
+!
+[0]
+? -> #jump // This one will not be printed
+
+| << #jump
+<print>
+```
+The `?` checks if the value that enters from the top is different than 0 / NULL / false.  
+Unlike a normal jump, this one will not return after executing the code at its jump target location.
+
+```
+| << #conditionalJump
+=
+["I'm here 1"]
+<print>
+
+!
+["I'm here 2"]
+<print>
+| -> #conditionalJump
+=
+["I'm here 3"]
+<print>
+=
+[1]
+? -> #conditionalJump
+=
+["I'm here 4"] // This part will never get executed
+<print>
+```
+Logical comparators like `<eq>`, `<neq>`, `<les>` etc. are useful for conditional jumps and return either true or false.
+
 ### Other data types
-Work in progress :P
+We can create strings simply by using quotation marks `""` inside a value block.  
+```
+!
+["This is a string!"]
+<print>
+```
+
+`<input>` by default returns a string but often we want the user to input a number.  
+For this conversion functions are used like `<tonum>`.  
+Example:  
+```
+!
+["Input a number: "]
+<input>
+<tonum>
+| -> #num
+<add> <- #num
+<print> // input * 2
+```
+
+There are also arrays. To create an array use curly brackets inside a value block.  
+```
+!
+[{5, 4, "text", 7}]
+<print>
+```
+You can convert strings into arrays and back using `<toarr>` and `<tostr>`.  
+Example:
+```
+!
+["string"]
+<toarr>
+<print> // [ 's', 't', 'r', 'i', 'n', 'g' ]
+<tostr>
+<print> // string
+```
+### Working with arrrays
+You can set and get elements of an array using `<at>` and `<set>`.
+`<at>` gets the array from the top and the index from the side.  
+```
+!
+[1]
+| -> #index
+
+!
+[{5,4,3,2,1}]
+|
+<at> <- #index
+|
+<print> // 4
+```
+`<set>` also gets the array from the top.  
+It will work differently depending on what is the input on it's side.  
+If a number or a string enters from its side it will append it to the end of the array.
+```
+!
+["new"]
+| -> #el1
+
+!
+[5]
+| -> #el2
+
+!
+[{1,2,3}]
+<set> <- #el1
+<print> //[ 1, 2, 3, 'new' ]
+<set> <- #el2
+<print> //[ 1, 2, 3, 'new', 5 ]
+```
+If an array enters from it's side, it will be used to specify the key / index and the value to write into the array. That input array has to be the length of 2.  
+```
+!
+[{4,"overwrite!"}]
+| -> #newVal
+
+!
+[{9,8,7,6,5,4,3,2,1}]
+<set> <- #newVal
+<print> // [ 9, 8, 7, 6, 'overwrite!', 4, 3, 2, 1 ]
+```
 
 
 ## Functions
@@ -125,4 +245,9 @@ First argument enters from the top, second from the side.
 ### &lt;tonum&gt; &lt;tostr&gt; &lt;toarr&gt;
 Convert input to a number / string / array.  
 The input enters from the top.
-
+## Debugger
+To use the debuger add use the `--debug` argument.  
+Define breakpoints in the `--break` argument.  
+```
+node . ./fibonacci.telep --debug --break:15,19
+```
--- teleport.js
+++ teleport.js
@@ -1,11 +1,35 @@
 const prompt = require("prompt-sync")({ sigint: true });  
 const fs = require('fs');
+
+
 async function main() {
+    let debug = false;
+    let maxLogHeight = 0;
     try{
+        function error(line, lineI, reason) {            
+            throw `ERROR Line ${lineI+1}: ${reason}\n${lineI + 1} |${line}`;     
+        }
+
         let path = process.argv[2];// || "./fibonaci.factory";
         if (path == undefined) {
             throw "ERROR: No file provided.";
         }
+        
+        let breakpoints = [];
+
+        for (let arg = 3; arg < process.argv.length; arg++) {
+            let argument = process.argv[arg];
+            if (argument == "--debug") {
+                debug = true;            
+            }
+            else if (argument.startsWith("--break:")) {
+                breakpoints = argument.split(":")[1].split(",");
+            }
+            else
+                throw `ERROR: Invalid parameter ${argument}`;
+
+        }
+
         let code = await new Promise((res)=>{
             fs.readFile(path, 'utf8', (err, data) => {
                 if (err) {
@@ -15,14 +39,17 @@ async function main() {
                 res(data);
             });
         });
+        let debugCode = code;
         let vars = [];
         let tpIn = [];
         let tpOut = [];
         let starts = [];
+        let outLog = "";
         let regex = /\/\/.*\n/g;
+        let breakpoint = true;
         code = code.replace(/\r\n/g,"\n");
         code += "\n"; // don't ask
-        code = code.replace(regex, "\n") //wywal komentarze
+        code = code.replace(regex, "\n") //remove comments
         let lines = code.split("\n");
         for (let i = 0; i < lines.length; i++) {
             let line = lines[i];
@@ -36,10 +63,10 @@ async function main() {
                 let val = line.split("[")[1].split("]")[0];
                 
                 if (val == '') {
-                    throw `ERROR Line ${i+1}: No value found inside square brackets.`;                    
+                    error (line,i,"No value found inside square brackets.");                    
                 }
                 if (!line.includes("]")) {
-                    throw `ERROR Line ${i+1}: No closing bracket.`;                    
+                    error (line,i,"No closing bracket.");                 
                 }
 
                 val = val.replace("{","[").replace("}","]");
@@ -53,7 +80,7 @@ async function main() {
             else if (line.startsWith("=")) { }
             else if (line.startsWith("?")) { }
             else {
-                throw `ERROR Line ${i+1}: Invalid character.`;    
+                error (line,i,"Invalid character.");         
             }
                 
             let arrowCount = 0
@@ -61,7 +88,7 @@ async function main() {
                 arrowCount++;
                 let tpName = line.split("#")[1];
                 if (tpName == undefined) {
-                    throw `ERROR Line ${i+1}: "#" symbol missing after ->.`;                    
+                    error (line,i,"\"#\"---- symbol missing after ->.");                           
                 }
 
                 tpOut[i] = {name: tpName, value: undefined};
@@ -70,7 +97,7 @@ async function main() {
                 arrowCount++;
                 let tpName = line.split("#")[1];
                 if (tpName == undefined) {
-                    throw `ERROR Line ${i+1}: "#" symbol missing after <-.`;                    
+                    error (line,i,"\"#\" symbol missing after <-.");                    
                 }
 
                 tpIn[i] = {name: tpName, sendSignal: false};
@@ -79,18 +106,18 @@ async function main() {
                 arrowCount++;
                 let tpName = line.split("#")[1];
                 if (tpName == undefined) {
-                    throw `ERROR Line ${i+1}: "#" symbol missing after <<.`;                    
+                    error (line,i,"\"#\" symbol missing after <<.");                      
                 }
                 
                 tpIn[i] = {name: tpName, sendSignal: true};
             }
             
             if (line.includes("#") && arrowCount == 0) {
-                throw `ERROR Line ${i+1}: arrow missing before "#" symbol.`;                   
+                error (line,i,"arrow missing before \"#\" symbol.");                      
             }
 
             if (arrowCount != 1 && arrowCount != 0) {
-                throw `ERROR Line ${i+1}: too many arrows.`;                      
+                error (line,i,"too many arrows.");                   
             }
 
 
@@ -106,23 +133,26 @@ async function main() {
         function checkFunctions(line, i, val, prevVal) {
             let convIn = prevVal;
             if (line.startsWith("<print>")) {
-                console.log(val);       
+                if (debug)                     
+                    outLog += `${val}\n`;
+                else
+                    console.log(val);       
             }
             if (line.startsWith("<input>")) {
+                if (typeof(val) != "string") {
+                    error (line,i,"Variable is not a string");                      
+                }                
                 return prompt(val);
             }
             if (line.startsWith("<at>")) {
                 if (typeof(prevVal) != "object") {
-                    throw `ERROR Line ${i+1}: Variable is not a table`;
-                }
-                if (prevVal[val] == undefined) {
-                    throw `ERROR Line ${i+1}: Wrong table index / key`;
+                    error (line,i,"Variable is not a table");       
                 }
                 return prevVal[val];
             }
             if (line.startsWith("<set>")) {
                 if (typeof(prevVal) != "object") {
-                    throw `ERROR Line ${i+1}: Variable is not a table`;
+                    error (line,i,"Variable is not a table");   
                 }
                 
                 if (typeof(val) != "object")
@@ -134,7 +164,7 @@ async function main() {
                 else
                 {
                     if (!val || val.length != 2) {
-                        throw `ERROR Line ${i+1}: Wrong key value table for <set>`;
+                        error (line,i,"Wrong key value table for <set>");
                     }             
                     let tab = JSON.parse(JSON.stringify(prevVal));
                     tab[val[0]] = val[1];
@@ -152,7 +182,7 @@ async function main() {
             }
             if (line.startsWith("<div>")) {
                 if (val == 0) {
-                    throw `ERROR Line ${i+1}: Division by 0`;
+                    error (line,i,"Division by 0");
                 }
                 return convIn / val;
             }
@@ -162,16 +192,16 @@ async function main() {
             if (line.startsWith("<neq>")) {
                 return convIn != val;
             }
-            if (line.startsWith("<les>")) {
+            if (line.startsWith("<les>") || line.startsWith("<less>")) {
                 return convIn < val;
             }
-            if (line.startsWith("<mor>")) {
+            if (line.startsWith("<mor>") || line.startsWith("<more>")) {
                 return convIn > val;
             }
-            if (line.startsWith("<lesq>")) {
+            if (line.startsWith("<lesq>") || line.startsWith("<lessq>")) {
                 return convIn <= val;
             }
-            if (line.startsWith("<morq>")) {
+            if (line.startsWith("<morq>") || line.startsWith("<moreq>")) {
                 return convIn >= val;
             }
             if (line.startsWith("<tonum>")) {
@@ -192,16 +222,99 @@ async function main() {
                     return toString(val);
             }
         }
+        function valBlockDebug(i, val) {
+            let debugLines = debugCode.split("\n");
+            debugLines[i] = debugLines[i].replace(/(?<=\[).*(?=\])/, val);
+            debugCode = debugLines.join("\n");
+        }
+        function doDebug(i,val,prevVal) { 
+            let logAll = "";
+            if (breakpoints.includes(`${i + 1}`)) {
+                breakpoint = true;
+            }
+
+            if (val == undefined) {
+                val = "NULL";
+            }
+            if (prevVal == undefined) {
+                prevVal = "NULL";
+            }
+            if (tpIn[i] == undefined) {
+                prevVal = "NULL";
+            }
+            else if(!tpIn[i].sendSignal)
+            {
+                let tp = findTp(tpOut,tpIn[i].name);
+                if (tp) {
+                    prevVal = tp.value;                    
+                }
+            }
+            logAll += outLog + "\n";
+            logAll += "-------------------\n";
+
+            let debugLines = debugCode.split("\n")
+
+            let start = i - 5
+            let end = i + 5;
+
+            for (let j = start; j <= end; j++) {
+                let max = Math.floor(Math.log10(debugLines.length) + 1);
+                if (j >= debugLines.length || j < 0) {
+                    let line = "";
+                    for (let l = 0; l <= max; l++) {
+                        line = " " + line;
+                    }
+                    logAll += line + "\n";
+                    continue;
+                }                
+                let line = debugLines[j];
+                let here = Math.floor(Math.log10(j + 1) + 1);
+
+                line = line.replace(/\[(.*?)\]/,"\u001b[34m[\u001b[31m$1\u001b[34m]\u001b[0m")
+                if (j == i) {
+                    line = "\u001b[31m>\u001b[0m" + line;
+                }
+                else {
+                    line = " " + line;
+                }
+                for (let l = 0; l <= max - here; l++) {
+                    line = " " + line;
+                }
+                line = "\u001b[1;33m" + (j + 1) + "\u001b[0m" + line;
+                line = line.replace(/(\#.+?)((\/\/)|$)/m,"\u001b[35m$1\u001b[0m$2")
+                line = line.replace(/(\<\-|\-\>|\<\<)/,"\u001b[36m$1\u001b[0m")
+                line = line.replace(/(\<.*?\>)/,"\u001b[34m$1\u001b[0m")
+                line = line.replace(/(?<a>.*?)(?<place>)(?<b>\/\/.*)/,"$<a>\u001b[3;32m$<b>\u001b[0m")
+                logAll += line + "\n"
+            }
+            logAll += "-------------------" + "\n";
+            logAll += `VALUE: ${val}` + "\n";
+            logAll += `SIDE VAL: ${prevVal}` + "\n";
+            logAll += "-------------------" + "\n";
+            logAll = "\033[0J" + logAll.replace("\n", "\n\033[0J");
+            //console.log("\033[2J") //Clear screen            
+            console.log(logAll);
+            if (breakpoint) {                
+                let p = prompt(`Enter - next line | C - continue | > `);
+                if (p == "c" || p == "C") {
+                    breakpoint = false;
+                }
+            }
+            console.log("\033[0;0H"); // Go to first line
+            maxLogHeight = Math.max(maxLogHeight, logAll.split("\n").length);
+            
+        }
         function execute(lines, lineNum, val = undefined) {    
             let prevVal = undefined;
             for (let i = lineNum; i < lines.length; i++) {
                 let line = lines[i];
                 line = line.trim();
                 if (line == "")
-                    return;
-                if (line.startsWith("="))
-                    val = undefined;
+                    return;                    
+
                 
+                if (line.startsWith("="))
+                    val = undefined;                
                 
                 if (line.startsWith("?"))
                 {
@@ -209,15 +322,21 @@ async function main() {
                         if (tpOut[i] != undefined) {
                             tpOut[i].value = val;
                             let tp = findTp(tpIn,tpOut[i].name);    
-                            let l = tpIn.indexOf(tp);
-                            if (tp.sendSignal) {
-                                execute(lines, l, val);
+                            let l = tpIn.indexOf(tp);                            
+                            if (tp != undefined) {                                          
+                                if (tp.sendSignal) {
+                                    execute(lines, l, val);
+                                }
+                                else {
+                                    error (line,i,"Cannot jump from Line ${i}. Use << instead of <-");
+                                }
                             }
-                            else
-                                throw `ERROR Line ${l+1}: Cannot jump from Line ${i}. Use << instead of <-`;
+                            else {
+                                error (line,i,`${tpOut[i].name} doesn't have an teleport in (<- / <<) with the same name`);
+                            }
                         }
                         else {
-                            throw `ERROR Line ${i+1}: No conditional destination`;
+                            error (line,i,"No conditional destination");
                         }
                         return;
                     }
@@ -228,8 +347,18 @@ async function main() {
                     if (val == undefined)
                         val = vars[i];
                     else
+                    {
                         vars[i] = val;
+                        if (debug) {
+                            valBlockDebug(i, val);
+                        }
+                    }
                 }
+                
+                if (debug) {
+                    doDebug(i, val, prevVal);
+                }
+                
                 if (tpOut[i] != undefined) {
                     tpOut[i].value = val;
                     let tp = findTp(tpIn,tpOut[i].name);
@@ -239,28 +368,37 @@ async function main() {
                             execute(lines, l, val);
                         }
                     }
+                    else
+                        error (line,i,`${tpOut[i].name} doesn't have an teleport in (<- / <<) with the same name`);
                 }
                 prevVal = val;
                 if (tpIn[i] != undefined) {
                     if (!tpIn[i].sendSignal) {
                         let tp = findTp(tpOut,tpIn[i].name);     
                         if (tp == undefined) {
-                            throw `ERROR Line ${i+1}: ${tpIn[i].name} doesn't have an tpOut with the same name`;
+                            error (line,i,`${tpIn[i].name} doesn't have an teleport out (->) with the same name`);
                         }
                         val = tp.value         
                     }
-                }
+                }               
+
                 let ret = checkFunctions(line, i, val, prevVal);
                 if (ret != undefined) {
                     val = ret;
                 }
             }
         }
+        if (starts.length == 0) {
+            console.error(`WARNING: No program start "!" found.`);                        
+        }
         for (let st of starts) {
             execute(lines, st.line);
         }  
     } catch (error) {
         console.error(error);
+    }
+    if (debug) {
+        console.log("\033["+maxLogHeight+";0H");    
     }
 }
 main();    
